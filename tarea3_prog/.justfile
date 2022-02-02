ec897a8bcda386aece6a40f4b643480836a1276d
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-02-01 21:49:25 -0600

    Single justfile

--- tarea3_prog/.justfile
+++
@@ -1,4 +0,0 @@
-set dotenv-load := false
-
-@run file:
-    g++ {{file}} -o /tmp/out && /tmp/out
--- tarea4_prog/switch2.cpp
+++
@@ -1,7 +0,0 @@
-/*
-  Daniel Alejandro Osornio López -- Programación Avanzada
-  
-  Utilizando la estructura Switch, realice un programa en el cual me muestre un menú
-  de la carta del restaurante, el usuario puede realizar solicitud de diferentes platos y
-  bebidas y al final obtener la cuenta a pagar.
-*/
